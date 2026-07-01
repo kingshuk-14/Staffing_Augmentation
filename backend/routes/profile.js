971e00d25cc2bbf2f3d79678b0965bbeeb670999
kingshuk-14 <kingshukghosh2005@gmail.com>
2026-07-01 16:29:26 +0530

    Fix bcryptjs module not found error

--- backend/routes/profile.js
+++ backend/routes/profile.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const bcrypt = require('bcryptjs');
+const bcrypt = require('bcrypt');
 const crypto = require('crypto');
 const db = require('../db');
 const { authenticateToken } = require('../middleware/auth');
